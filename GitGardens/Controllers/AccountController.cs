f55b175d649ed19a43045e33e47f82a3f3f006af
Quen Drew <itsquendrew@gmail.com>
2026-03-07 12:38:11 +0200

    feat: Implemented Registration and Login Views and Logic

---
+++ GitGardens/Controllers/AccountController.cs
@@ -0,0 +1,179 @@
+﻿using GitGardens.Data;
+using GitGardens.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+
+namespace GitGardens.Controllers
+{
+
+    /*
+       Title: ASP.NET Core MVC CRUD - NET 6 MVC CRUD  
+       Operations Using Entity Framework Core and SQL Server  
+       Author: Sameer Saini  
+       Date: 2 years ago
+       Date accessed: 05/03/2026
+       Code version : 1  
+       Availability: https://youtu.be/2Cp8Ti_f9Gk?feature=shared
+    */
+
+    /*
+       Title: ASP.NET 8 MVC Tutorial for Beginners - C# web development made easy  
+       Operations Using Entity Framework Core and SQL Server  
+       Author: tutorialsEU  
+       Date: 1 years ago
+       Date accessed: 05/03/2026
+       Code version : 1  
+       Availability: https://youtu.be/xuFdrXqpPB0?feature=shared
+    */
+
+    /*
+       Title: Asp.Net Core MVC & Identity UI - User Registration and Login
+       Author: CodAffection  
+       Date: 2 years ago
+       Date accessed: 05/03/2026
+       Code version : 1  
+       Availability: https://youtu.be/wzaoQiS_9dI?feature=shared
+    */
+
+
+    public class AccountController : Controller
+    {
+
+        private readonly GitGardensDBContext context;
+        private readonly IPasswordHasher<User> passwordHasher;
+
+        // DbContext Constructor
+        public AccountController(GitGardensDBContext Context, IPasswordHasher<User> PasswordHasher)
+        {
+            context = Context;
+            passwordHasher = PasswordHasher;
+        }
+
+        //////////////////////////////////////////////////////////Register//////////////////////////////////////////////////////////
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(Register model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Check if Email is already taken
+                if (context.Users.Any(u => u.Email == model.Email))
+                {
+                    TempData["Email"] = "Email Already in Use";
+                    return View(model);
+                }
+
+                // Create User
+                var user = new User
+                {
+                    FullName = model.FullName,
+                    Email = model.Email,
+                    Password = passwordHasher.HashPassword(null, model.Password),  // Hash Password
+                    RoleID = model.RoleID
+                };
+
+                context.Users.Add(user);
+                await context.SaveChangesAsync();   // Save Changes
+
+                // Redirect to Login After Successful Registeration
+                return RedirectToAction("Login", "Account");
+            }
+            return View();
+        }
+
+        //////////////////////////////////////////////////////////User Profile//////////////////////////////////////////////////////////
+        [HttpGet]
+        public async Task<IActionResult> Profile()
+        {
+            var email = User.FindFirstValue(ClaimTypes.Email);              // Retrieve the Logged in user email
+
+            if (email == null)
+            {
+                return NotFound();
+            }
+
+            var user = await context.Users.Include(u => u.Role).FirstOrDefaultAsync(u => u.Email == email);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        //////////////////////////////////////////////////////////Login + Logout//////////////////////////////////////////////////////////
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(Login model)
+        {
+            // Check Model State
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = context.Users.Include(u => u.Role)
+                .FirstOrDefault(u => u.Email == model.Email);
+
+            // Validate Credentials
+            if (user == null || !passwordHasher.VerifyHashedPassword(user, user.Password, model.Password).Equals(PasswordVerificationResult.Success))
+            {
+                TempData["Fail"] = "Invalid Email or Password";
+                return View(model);
+            }
+
+            // Create user claims - Pieces of information about this user - Creates login session for eah respective user
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.UserID.ToString()),   // User Unique Identitfier
+                new Claim(ClaimTypes.Name, user.FullName),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Role, user.Role.RoleName)  // User role for Role based Access control
+            };
+
+            // Caims Identity Object representing autheticated user identity
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            // Sign user in by creating an Authenticatoon Cookie
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+
+            // Redirect Users Based of Role
+            switch (user.Role.RoleName)
+            {
+                case "Admin":
+                    return RedirectToAction("Dashboard", "Admin");
+                case "User":
+                    return RedirectToAction("Dashboard", "User");
+                default:
+                    return RedirectToAction("Index", "Home");
+            }
+
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            return RedirectToAction("Login", "Account");
+        }
+
+
+    }
+}
