928eec7b1f18123c5ee7f8501beb62012a22f100
Dean Pitts <ST10449407@myemeris.edu.za>
2026-05-05 14:10:22 +0200

    feat: addition of edit, delete and profile views. Improvement of account controller

---
+++ GitGardens/Models/EditProfileViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GitGardens.Models
+{
+    public class EditProfileViewModel
+    {
+        [Required(ErrorMessage = "Full Name is Required")]
+        [StringLength(150)]
+        public string FullName { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; } // Kept for display/identification
+
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Password must be at least 6 characters")]
+        [DataType(DataType.Password)]
+        public string? NewPassword { get; set; } // Nullable, so it's optional
+    }
+}
